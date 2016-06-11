3bd06ef09077282d8b045b4376ee9e177168ac45
Mike Yates <mike@yatesdev.com>
2016-06-11 01:34:25 -0400

    Added song model

--- server/song/models.py
+++ server/song/models.py
@@ -3,3 +3,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Song(models.Model):
+	title = models.CharField(max_length=100, default='')
+	track_id = models.CharField(max_length=150, default='')
+	album_art_url = models.CharField(max_length=200, default='')
+	artist_name = models.CharField(max_length=150, default='')
+	def __unicode__(self):
+		return self.title + " - " + self.artist_name
--- server/songrequests/models.py
+++ server/songrequests/models.py
@@ -1,18 +1,14 @@
 from __future__ import unicode_literals
-
 from django.db import models
 from users.models import *
+from song.models import *
 
 # Create your models here.
 
-class Request(models.Model):
-	
+class SongRequest(models.Model):
+	song = models.ForeignKey(Song)
+	user = models.ForeignKey(User)
+	date_requested = models.DateTimeField(auto_now_add=True)
+	likes = models.PositiveIntegerField(default=0)
 	def __unicode__(self):
-		return ""
-	# requestlist = models.ForeignKey(RequestList)
-	# song = models.ForeignKey(Song)
-	# user = models.ForeignKey(User)
-	# date_requested = models.DateTimeField(auto_now_add=True, blank=True, null=True)
-	# likes = models.PositiveIntegerField(null=True, default=0)
-	# def __unicode__(self):
-	# 	return self.song.title + " - " + self.user.username
+		return self.song.title + " - " + self.user.username
