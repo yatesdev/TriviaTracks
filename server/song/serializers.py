7af9f460dfdc0e4c1312da675a9cdd8597804b19
Mike Yates <mike@yatesdev.com>
2016-06-11 07:05:39 -0400

    fully expanded Song metadata

--- server/song/serializers.py
+++ server/song/serializers.py
@@ -1,7 +1,18 @@
 from rest_framework import serializers
 from models import *
 
+class AlbumMetadataSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = AlbumMetadata
+		fields = ('spotifyID','name','href','album_type','uri')
+
+class ArtistMetadataSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = ArtistMetadata
+		fields = ('spotifyID','name','href','uri')
 class SongMetadataSerializer(serializers.ModelSerializer):
+	album = AlbumMetadataSerializer(read_only=True)
+	artist = ArtistMetadataSerializer(read_only=True)
 	class Meta:
 		model = SongMetadata
 		fields = ('name',
@@ -20,5 +31,7 @@ class SongSerializer(serializers.HyperlinkedModelSerializer):
 	metadata = SongMetadataSerializer(read_only=True)
 	class Meta:
     		model = Song
-        	fields = ('metadata',)
+        	fields = ('metadata','playlist')
+
+
 
