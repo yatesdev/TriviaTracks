682ab5ef825e30026c4b82d03a7fd4d8ac95c48b
Mike Yates <mike@yatesdev.com>
2016-06-11 00:04:27 -0400

    Reorganizing Project for Client/API methodology

--- home/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Song,Location,Request,RequestList,UserProfile
-
-# Register your models hereself.
-admin.site.register(Song)
-admin.site.register(Location)
-admin.site.register(Request)
-admin.site.register(RequestList)
-admin.site.register(UserProfile)
