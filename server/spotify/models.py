afd141a22c760f7f6491fc0e9ab4d1f305e7a366
Mike Yates <mike@yatesdev.com>
2016-06-12 22:19:07 -0400

    Add Search Log

--- server/spotify/models.py
+++ server/spotify/models.py
@@ -1,5 +1,8 @@
 from __future__ import unicode_literals
-
 from django.db import models
+from user.models import *
 
-	
+class SearchLog(models.Model):
+	query = models.CharField(max_length=255)
+	user = models.ForeignKey(User)
+	datetime = models.DateTimeField(auto_now_add=True)
