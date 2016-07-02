410d4d4ce72ea4cb47615c79af02dffaba0326f6
Mike Yates <mike@yatesdev.com>
2016-07-02 01:29:34 -0400

    Removed vestigal react-router code from boilerplate

--- client/src/js/routes.js
+++ client/src/js/routes.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { Route, IndexRoute, Redirect } from 'react-router';
 
 import App from './components/App';
-import FriendListApp from './containers/FriendListApp/FriendListApp';
+import TriviaTracksApp from './containers/TriviaTracksApp/TriviaTracksApp';
 import NotFoundView from './views/NotFoundView';
 
 export default (
   <Route path="/" component={App}>
-    <IndexRoute component={FriendListApp} />
+    <IndexRoute component={TriviaTracksApp} />
     <Route path="404" component={NotFoundView} />
     <Redirect from="*" to="404" />
   </Route>
