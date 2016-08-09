0561fc71f65ce8819dc4caeece078b6ebb7c75ee
Mike Yates <mike@yatesdev.com>
2016-08-08 23:09:51 -0400

    Finally Found a decent Boilerplate.

--- client/src/js/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as AddFriendInput } from './AddFriendInput/AddFriendInput';
-export { default as FriendList } from './FriendList/FriendList';
-export { default as SongList } from './SongList/SongList';
-export { default as RequestList } from './RequestList/RequestList';
-export { default as Header } from './Header/Header';
