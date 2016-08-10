8ab229fa0017ace63fcce2d37b97c1b4fd43e9fa
Mike Yates <mike@yatesdev.com>
2016-08-10 04:56:55 -0400

    Header to ES6 class

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,21 +1,42 @@
 import React from 'react'
 import { IndexLink, Link } from 'react-router'
 import classes from './Header.scss'
+import LogoImage from '../../static/img/TriviaTracksLogo.png'
 
-export const Header = () => (
-  <div className="page-container">
-    <div className="header">
-      <div className="pull-left full-height visible-sm visible-xs">
-        <div className="sm-action-bar">
-          <a className="btn-link toggle-sidebar">
-            <span className="icon-set menu-hamburger p-b-15">
-              <i className="fa fa-navicon"></i>
-            </span>
-          </a>
+export default class Header extends React.Component {
+  render() {
+    return (
+      <div className="page-container">
+        <div className="header">
+          <div className="pull-left full-height visible-sm visible-xs">
+            <div className="sm-action-bar">
+              <a className="btn-link toggle-sidebar">
+                <span className="icon-set menu-hamburger p-b-15">
+                  <i className="fa fa-navicon"></i>
+                </span>
+              </a>
+            </div>
+          </div>
+          <div className="pull-right full-height visible-sm visible-xs">
+            <div className="sm-action-bar">
+              <a href="#" class="btn-link p-t-15" data-toggle="search" >
+                <span class=""><i class="pg-search"></i></span>
+              </a>
+            </div>
+          </div>
+          <div className="pull-left sm-table">
+            <div className="header-inner">
+              <div className="brand inline">
+                <img
+                  src={ LogoImage }
+                  width="93"
+                  height="25" />
+              </div>
+              <a href="#" className="search-link" data-toggle="search"><i className="fa fa-search"></i>Type anywhere to <span className="bold">search</span></a>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
-)
-
-export default Header
+    );
+  }
+}
