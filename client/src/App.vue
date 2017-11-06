816244414784811951b1bb4a7dbbdd94d0cd79a3
Mike Yates <mike@yatesdev.com>
2017-11-06 01:07:05 -0500

    Working on adding NavBar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,23 +1,31 @@
 <template>
   <div id="app">
+    <NavBar/>
     <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
 
 <script>
+import NavBar from './components/NavBar';
+
 export default {
   name: 'app',
+  components: {
+    NavBar,
+  },
 };
 </script>
 
 <style>
+body{
+  margin: 0;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ client/src/components/NavBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="header">
+    Sup
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+};
+</script>
+
+<style lang="scss" scoped>
+  .header {
+    height: 50px;
+    color: white;
+    background-color: blue;
+  }
+</style>
