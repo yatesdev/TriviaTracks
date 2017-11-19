cc0d201094035e19def0345aa0dd74b0e375a254
Mike Yates <mike@yatesdev.com>
2017-11-19 15:01:38 -0500

    Add basics for passwords
    Also moved basic calls to the UserModel rather than have in controllers.

--- api/models/UserModel.js
+++ api/models/UserModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const UserSchema = new mongoose.Schema({
   first_name: {
@@ -6,6 +7,15 @@ const UserSchema = new mongoose.Schema({
   },
   last_name: {
     type: String,
+  },
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
   },
   created_on: {
     type: Date,
@@ -16,4 +26,37 @@ const UserSchema = new mongoose.Schema({
 UserSchema.virtual('full_name')
   .get(() => `${this.first_name} ${this.last_name}`);
 
-export default  mongoose.model('User', UserSchema);
+UserSchema.pre('save', async (next) => {
+  try {
+    const user = this;
+    if (user.isModified('password') || user.isNew) {
+      user.password = await bcrypt.hash(user.password, 10);
+    }
+  } catch (err) {
+    next(err);
+  }
+  next();
+});
+
+UserSchema.methods.comparePassword = function comparePassword(password, cb) {
+  bcrypt.compare(password, this.password, (err, isMatch) => {
+    if (err) {
+      cb(err);
+    }
+    cb(null, isMatch);
+  });
+};
+
+const UserModel = mongoose.model('User', UserSchema);
+
+UserModel.getAll = () => UserModel.find({});
+
+UserModel.addUser = (userToAdd) => userToAdd.save();
+
+UserModel.getOne = (id) => UserModel.findById(id);
+
+UserModel.updateOne = (id, data) => UserModel.findOneAndUpdate({ _id: id }, data, { new: true });
+
+UserModel.removeOne = (id) => UserModel.findOneAndRemove({ _id: id });
+
+export default UserModel;
