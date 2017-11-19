8aae02b529f131d976493ac8f5eae004dc6c61b5
Mike Yates <mike@yatesdev.com>
2017-11-19 12:59:51 -0500

    Remove extra imports

--- api/models/index.js
+++ api/models/index.js
@@ -1,8 +1,3 @@
-import SongModel from './SongModel';
-import UserModel from './UserModel';
-import RequestModel from './RequestModel';
-import LikeModel from './LikeModel';
-
 export { default as Song } from './SongModel';
 export { default as User } from './UserModel';
 export { default as Request } from './RequestModel';
