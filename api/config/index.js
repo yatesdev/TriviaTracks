000ca3903b6a1680e237535f1af15b5352071aca
Mike Yates <mike@yatesdev.com>
2017-11-19 14:42:06 -0500

    lint fixes

--- api/config/index.js
+++ api/config/index.js
@@ -1,11 +1,11 @@
 export default {
-	server: {
-		port: 8230
-	},
-	connection: {
-		database: "mongodb://localhost:27017"
-	},
-	secret: {
-		key: "sillyRabbitTricksAreForKids"
-	}
-}
+  server: {
+    port: 8230,
+  },
+  connection: {
+    database: 'mongodb://localhost:27017',
+  },
+  secret: {
+    key: 'sillyRabbitTricksAreForKids',
+  },
+};
